ab22b74ff055883b405c6eadbe6ad5980df6ebec
Tim Legrand <timlegrand.perso@gmail.com>
2015-12-28 01:15:15 +0100

    export node.js environment variable when needed

--- serve.sh
+++ serve.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
 
+### Check requirements
+type node > /dev/null 2>&1 || { echo "Trying to update PATH with Node.js..." ; export PATH=$PATH:/tmp/node-v4.2.3-linux-x64/bin ; }
+type node > /dev/null 2>&1 || { echo "Node not found. Aborting" ; exit 1 ; }
+
 bundle exec jekyll serve --watch --config _config.yml,_config_dev.yml
